154beabdcd6456d2f803ca728abb84a8d522c346
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-09-14 09:46:55 +0200

    Local storage

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
-    <ToastContainer autoClose={1200}/>
-  </React.StrictMode>
+    <ToastContainer autoClose={1200} />
+  </>
 );
